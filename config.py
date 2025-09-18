6c3384d8d5f92192bf07971381d7ab77660b5a89
Michael mo <mordohmichael9@gmail.com>
2025-09-18 12:31:10 +0300

    predict_review_by_text_mission

---
+++ config.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
