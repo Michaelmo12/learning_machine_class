b9b835393f7af6945e690fb32649f8a1f6ed8d7a
Michael <mordohmichael9@gmail.com>
2025-10-16 23:06:34 +0300

    minor change

--- config.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import re
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
